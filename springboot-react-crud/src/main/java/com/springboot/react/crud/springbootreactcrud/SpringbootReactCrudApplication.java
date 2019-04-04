cd876d8ac58f4dec30b56d56f7fce3bfabd52bda
Syed Omer Afzal Gillani <syed.omer.afzal.gillani@cybercom.com>
2019-04-04 12:36:02 +0200

    actuator and hal browser spring boot

---
+++ springboot-react-crud/src/main/java/com/springboot/react/crud/springbootreactcrud/SpringbootReactCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.react.crud.springbootreactcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactCrudApplication.class, args);
+	}
+
+}
