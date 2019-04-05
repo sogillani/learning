5e7be75e8a4e43efda833207c511391611672066
Omer Gillani <sogillani@gmail.com>
2019-04-05 16:35:31 +0200

    Learn aop

---
+++ in28minutes/spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/Dao1.java
@@ -0,0 +1,12 @@
+package com.in28minutes.spring.aop.springaop;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao1 {
+	
+	public String retriveSomething() {
+		return "Dao1";
+	}
+
+}
