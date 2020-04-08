7d0d1409fbc1fba9b7dadf137c1ace2d698175a9
Omer Gillani <sogillani@gmail.com>
2020-04-08 18:35:27 +0200

    remember me & log out

--- learn-spring-security/src/main/java/com/sog/spring/security/learnspringsecurity/security/ApplicationSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/sog/spring/security/learnspringsecurity/security/ApplicationSecurityConfiguration.java
@@ -14,6 +14,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.sog.spring.security.learnspringsecurity.security.ApplicationRole.*;
 
@@ -47,8 +51,23 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .and()
 //                .httpBasic();
                 .formLogin()
-                .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true);
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("mymd5key")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
